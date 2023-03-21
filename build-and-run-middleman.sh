a0bb1b31eab12c8d6d199bac88096656a2fc6966
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-21 10:55:46 +0100

    if docker network does not exist, create it

--- build-and-run-middleman.sh
+++ build-and-run-middleman.sh
@@ -7,5 +7,10 @@ mvn package #-Dspring.config.location=classpath:application.properties
 # Build the Docker image
 docker build -t middle-man .
 
+# Check if my-network exists, create it if it doesn't
+if ! docker network inspect my-network >/dev/null 2>&1; then
+    docker network create my-network
+fi
+
 # Run the Docker container
 docker run --rm --name middle-man --network my-network -p 9010:9010 middle-man
